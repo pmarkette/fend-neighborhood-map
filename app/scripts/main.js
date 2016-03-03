dcd4d5841f768ac3b01d4b78e786d13ee70f44de
pmarkette <paul@markette.net>
2016-03-03 15:50:28 -0800

    Move map js to main.js. Create test marker. Add bounce animation to marker.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,3 +10,28 @@ var ViewModel = function(first, last) {
 };
 
 ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+
+//Create the map and markers
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.8070047, lng: -122.2718144},
+    zoom: 15
+  });
+  var myLatLng = {lat: 37.8070047, lng: -122.2718144};
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'The Woods'
+  });
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+  marker.addListener('click', toggleBounce);
+  marker.setMap(map);
+}
+
